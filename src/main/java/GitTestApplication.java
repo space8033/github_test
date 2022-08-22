be22dc583d597012bfabc497ad4591754e089535
space8033 <space8237@naver.com>
2022-08-22 16:50:01 +0900

    edit

--- src/main/java/GitTestApplication.java
+++ src/main/java/GitTestApplication.java
@@ -1,5 +1,6 @@
 public class GitTestApplication {
     public static void main(String[] args) {
         System.out.println("To-Do-Application!");
+        System.out.println("To-Do-Application version 2!");
     }
 }
